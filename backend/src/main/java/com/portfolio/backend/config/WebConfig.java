2dbb43e2eba86fc52db7434cb8f918e84d8f0f31
shk <shk@dxtobe.com>
2026-02-06 17:01:57 +0900

    이미지 표시 관련 로직 추가

---
+++ backend/src/main/java/com/portfolio/backend/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.portfolio.backend.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.io.File;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${file.upload-dir}")
+    private String uploadDir;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        String path = uploadDir.endsWith(File.separator) ? uploadDir : uploadDir + File.separator;
+        
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("file:" + path);
+    }
+}
